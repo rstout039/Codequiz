68781573737801a76ff68a99c5a4d2e597dca27d
rstout039 <68915818+rstout039@users.noreply.github.com>
2020-10-09 11:21:35 -0400

    Create questions.js

---
+++ Codequiz/questions.js
@@ -0,0 +1,37 @@
+var questions = [
+    {
+    prompt: "Why is CSS important?",
+    choices: ["To add style to the webpage", "To add more pages", "it sounds cool"],
+    answer: "To add style to the webpage"
+    },
+    {
+    prompt: "What website do you refer to when making code?",
+    choices: ["Bootstrap", "Mycode.net", "codegenerator.com", "webdevelopernation.com"],
+    answer: "Bootstrap"
+    },
+    {
+    prompt: "Commonly used scripts DO NOT include:",
+    choices: ["html", "CSS", "Javascript", "Jquery", "None of the above"],
+    answer: "None of the above"
+    },
+    {
+    prompt: "What symbol is used for a jquery specific selector",
+    choices: ["$", "<>", "!", "[]", "{}" ],
+    answer: "$"
+    },
+    {
+    prompt: "What does API stand for? (put answer in lowercase)",
+    choioces: ["application programming interface", "automatic programmer intercom", "austrailian pragmatic isomorph"],
+    answer: "application programming interface"
+    },
+    {
+    prompt: "What comes first in the document object model",
+    choices: ["document", "attribute", "text", "title", "element"],
+    answer: "document"
+    },
+    {
+    prompt: "What does Javascript do?",
+    choices: ["add functionality to the webpage", "add style to the webpage", "speed up the webpage", "freeze the webpage"],
+    answer: "add funtionality to the webpage"
+    }
+]
